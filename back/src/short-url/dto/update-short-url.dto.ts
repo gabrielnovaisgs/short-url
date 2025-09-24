4a20f56282d4d70b40293b22290c7cec6909d39b
gabrielnovaisgs <gabrielnovaisgs@gmail.com>
2025-09-24 20:03:02 -0300

    feat: add bcrypt dependency and remove unused update-short-url DTO

--- back/src/short-url/dto/update-short-url.dto.ts
+++
@@ -1,5 +0,0 @@
-import { createShortUrlDtoSchema } from './create-short-url.dto';
-import z from 'zod';
-
-export const updateShortUrlDtoSchema = createShortUrlDtoSchema.partial();
-export type UpdateShortUrlDto = z.infer<typeof updateShortUrlDtoSchema>;
